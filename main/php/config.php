66bde720e2c1446e1f0a0337ec9a7a10794c14e0
Bryan Owen <160561345+thinkabtwen@users.noreply.github.com>
2024-11-04 08:02:33 +0700

    Add logout feature

--- main/php/config.php
+++ main/php/config.php
@@ -99,6 +99,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
     }
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['logout'])){
+    $_SESSION = array();
 
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    session_destroy();
+    header("Location: ../html/LoginPage.html");
+}
 
 ?>
