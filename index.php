2981b2e99039e94a84881e01b1eefa4741654977
linglungTzy <achmaddaffafazli@gmail.com>
2024-11-14 04:11:26 +0700

    fix: Updated index.php to access web app from root directory

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cyber Resource</title>
-</head>
-<body>
-<script> location.href='main/html/MainHomePage.html'; </script>
-</body>
-</html>
+<?php
+header("Location: main/html/MainHomePage.html");
+?>
